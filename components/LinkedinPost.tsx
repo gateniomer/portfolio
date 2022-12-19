18558e0bb10388a87f7c35d44b450b7a7626b602
gateniomer <gateniomer@gmail.com>
2022-12-19 17:01:44 +0200

    linkedin section

---
+++ components/LinkedinPost.tsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+type Props = {
+  iframeUrl:string,
+  postUrl:string
+}
+
+export const LinkedinPost = ({iframeUrl,postUrl}:Props) => {
+  return (
+    <div className="w-[400px] max-w-[100%]">
+      <iframe
+      src={iframeUrl}
+      className='shadow-lg w-full h-[400px] border-[5px] rounded-sm'>
+      </iframe>
+      <a href={postUrl}
+      target='_blank'
+      className='inline-block  rounded-sm text-white w-full text-center bg-blue-400 px-6 py-2'>
+        Keep reading on LinkedIn <FontAwesomeIcon icon={faLinkedin}/>
+      </a>
+    </div>
+  )
+}
+
+export default LinkedinPost;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,24 @@ import Header from '../components/Header';
 import Badge from '../components/Badge';
 import Card from '../components/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboard, faPaperPlane,faPhone,faFileWord,faFilePdf,faClock,faGraduationCap,faCodeBranch } from '@fortawesome/free-solid-svg-icons';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { 
+  faClipboard, 
+  faPaperPlane,
+  faPhone,
+  faFileWord,
+  faFilePdf,
+  faClock,
+  faGraduationCap,
+  faCodeBranch,
+  faStar,
+  faHippo, 
+  faPeopleGroup,
+  faEye,
+  faNewspaper} from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp,faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import ContributionsSlider from '../components/ContributionsSlider';
 import BrandIconSlider from '../components/BrandIconSlider';
+import LinkedinPost from '../components/LinkedinPost';
 
 export default function Home() {
   
@@ -63,17 +77,40 @@ export default function Home() {
           </div>
       </section>
 
-      <div>
-          <h2 className='text-4xl'>I'm Active on Linkedin</h2>
-          {/* Linkedin Posts */}
-      </div>
-      <section>
-        <h2>Real Life Achivements</h2>
+      <section className='mt-[100px] max-w-[1200px] mx-auto'>
+        <h2 className='text-4xl'>Active on LinkedIn <FontAwesomeIcon icon={faLinkedin}/></h2>
+        <div className='inline-flex gap-3 md:gap-10 mx-auto my-2'>
+            <span>
+              <FontAwesomeIcon icon={faPeopleGroup}/> 1k+ followers</span>
+            <span><FontAwesomeIcon icon={faEye}/> 30k+ impressions</span>
+            <span><FontAwesomeIcon icon={faNewspaper}/> 10+ posts</span>
+          </div>
+        <p className='my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, soluta.</p>
+        <div className='flex flex-col md:flex-row gap-5 justify-center place-items-stretch'>
+                <LinkedinPost 
+                iframeUrl='https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7005219495053037568'
+                postUrl='https://www.linkedin.com/posts/omer-gatenio_%D7%A7%D7%A6%D7%AA-%D7%A2%D7%9C-%D7%9C%D7%9E%D7%99%D7%93%D7%94-%D7%A2%D7%A6%D7%9E%D7%99%D7%AA-activity-7005219495694745600-swRI?utm_source=share&utm_medium=member_desktop'/>
+                <LinkedinPost 
+                iframeUrl='https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6991396249262276608'
+                postUrl='https://www.linkedin.com/posts/omer-gatenio_programming-selfstudy-fullstack-activity-6991396398243954688-q7vh?utm_source=share&utm_medium=member_desktop'/>
+                <LinkedinPost 
+                iframeUrl='https://www.linkedin.com/embed/feed/update/urn:li:share:6990412191774445568'
+                postUrl='https://www.linkedin.com/posts/omer-gatenio_juniordeveloper-roadmapping-activity-6990412192734932993-xnw8?utm_source=share&utm_medium=member_desktop'/>
+        </div>
+      </section>
+
+      <section className='mt-[100px] max-w-[1200px] mx-auto'>
+        <h2 className='text-4xl'>Real Life Achivements <FontAwesomeIcon icon={faStar}/></h2>
+        <p className='my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, soluta.</p>
+        
+      </section>
+
+      <section className='mt-[100px] max-w-[1200px] mx-auto'>
+        <h2 className='text-4xl'>Hey, I'm Omer <FontAwesomeIcon icon={faHippo}/></h2>
+        <p className='my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, soluta.</p>
+        
       </section>
       
-      <section>
-        <h2>About</h2>
-      </section>
       <section className='max-w-[400px]'>
         <h2 className='text-5xl font-bold'>Get in touch</h2>
         <p className='mt-5'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, dicta?</p>
