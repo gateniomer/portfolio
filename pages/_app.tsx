4e4fd30e4a856a5a15b0854d5c32cd37da38d7ab
gateniomer <gateniomer@gmail.com>
2022-12-20 13:57:34 +0200

    fixed thumbnail

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name = "author" content = "Omer Gatenio" />
     <meta name='og:title' content='Omer Gatenio PORTFOLIO'/>
-    <meta name='og:image' content={'assets/thumbnail.webp'}/>
+    <meta name='og:image' content={'https://gateniomer.github.io/portfolio/assets/thumbnail.webp'}/>
     <meta name='og:description' content='I am highly motivated to turn my ideas into reality and have a strong passion for planning, designing, and developing projects and initiatives that can benefit myself and others.'/>
     <meta name='og:url' content='https://gateniomer.github.io/portfolio/'/>
     <meta name='og:image:width' content='1200' />
