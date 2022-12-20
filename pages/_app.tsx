756ff8065a9a01110ca2c49c3746787d56fd6a8f
gateniomer <gateniomer@gmail.com>
2022-12-20 03:21:34 +0200

    fixed scaling issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (<>
     <Head>
     <title>Omer Gatenio - Frontend Developer</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
     <main className={poppins.className}>
       <Component {...pageProps} />
@@ -21,3 +21,4 @@ export default function App({ Component, pageProps }: AppProps) {
     </main>
   </>)
 }
+
