02f72c6938e56f281c3ad51015a4cf2e467120f1
gateniomer <gateniomer@gmail.com>
2022-12-25 16:53:22 +0200

    cleaned code

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
   theme: {
     extend: {
       animation:{
-        'mobile-menu-enter':'enter 0.3s ease-out forwards',
         'infiniteSlide':'infiniteSlideLeft 5s linear infinite',
         'grow':'grow 0.9s linear infinite'
       }
