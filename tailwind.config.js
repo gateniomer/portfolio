71f8b79cb166c74b2fdd4e58a7664aca35a0bd5d
gateniomer <gateniomer@gmail.com>
2022-12-25 16:20:30 +0200

    cleaned code

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,8 @@ module.exports = {
     extend: {
       animation:{
         'mobile-menu-enter':'enter 0.3s ease-out forwards',
-        'slide':'slide 25s linear infinite',
-        'slideBrands':'slideBrands 20s linear infinite',
         'infiniteSlide':'infiniteSlideLeft 5s linear infinite',
         'grow':'grow 0.9s linear infinite'
-      },gridTemplateRows: {
-        // Simple 16 column grid
-        '7': 'repeat(7, minmax(0, 1fr))',
       }
     },
   },
